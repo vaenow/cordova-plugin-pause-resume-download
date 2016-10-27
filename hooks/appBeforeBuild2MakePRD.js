62519344f4607ac6326bfe71527df6ee28faaad3
vaenow <luowenbiz@gmail.com>
2016-10-28 00:41:21 +0800

    fixed PRD for iOS; iOS断点续下载修复正常

--- hooks/appBeforeBuild2MakePRD.js
+++ hooks/appBeforeBuild2MakePRD.js
@@ -7,7 +7,40 @@ module.exports = function(context) {
     var path = context.requireCordovaModule('path'),
         fs = context.requireCordovaModule('fs'),
         shell = context.requireCordovaModule('shelljs'),
-        projectRoot = context.opts.projectRoot;
+        projectRoot = context.opts.projectRoot,
+        plugins     = context.opts.plugins || [];
+
+    // The plugins array will be empty during platform add
+    if (plugins.length > 0 && plugins.indexOf('cordova-plugin-pause-resume-download') === -1) {
+        return ;
+    }
+
+    var ConfigParser = null;
+    try {
+        ConfigParser = context.requireCordovaModule('cordova-common').ConfigParser;
+    } catch(e) {
+        // fallback
+        ConfigParser = context.requireCordovaModule('cordova-lib/src/configparser/ConfigParser');
+    }
+
+    var config      = new ConfigParser(path.join(context.opts.projectRoot, "config.xml")),
+        packageName = config.android_packageName() || config.packageName();
+
+    // replace dash (-) with underscore (_)
+    packageName = packageName.replace(/-/g , "_");
+    
+    console.info("Running android-install.Hook: " + context.hook + ", Package: " + packageName + ", Path: " + projectRoot + ".");
+
+    if (!packageName) {
+        console.error("Package name could not be found!");
+        return ;
+    }
+
+    var projectName = config.name();
+
+
+
+
 
     //--------------------Android-------------------
     if (context.opts.cordova.platforms.indexOf("android") !== -1) {
@@ -28,17 +61,21 @@ module.exports = function(context) {
 
         //--------------------iOS-------------------
     } else if (context.opts.cordova.platforms.indexOf("ios") !== -1) {
-        var targetDir = path.join(projectRoot, "platforms", "ios", "小爱学习圈", "plugins", "cordova-plugin-file-transfer");
+        var targetDir = path.join(projectRoot, "platforms", "ios", projectName, "plugins", "cordova-plugin-file-transfer");
+        console.log('ios targetDir', targetDir)
         var targetFiles = ["CDVFileTransfer.m"];
         var tmpls = [{
             source: "// create target file",
-            target: "if ([[NSFileManager defaultManager] fileExistsAtPath:filePath] == NO) { \r\n // /*Commented for PRD by LuoWen*/create target file"
+            target: "if ([[NSFileManager defaultManager] fileExistsAtPath:filePath] == NO) { \r\n\t\t// /*Commented for PRD by LuoWen*/ // create target file"
         }, {
             source: "// open target file for writing",
-            target: "} \r\n // /*Commented for PRD by LuoWen*/open target file for writing"
+            target: "} \r\n\t\t// /*Commented for PRD by LuoWen*/ // open target file for writing"
         }, {
             source: 'DLog(@"Streaming to file %@", filePath);',
-            target: '[self.targetFileHandle seekToEndOfFile]; \r\n DLog(@"Streaming to file %@", filePath/*Commented for PRD by LuoWen*/);'
+            target: '[self.targetFileHandle seekToEndOfFile]; \r\n\t\tDLog(@"Streaming append to file %@", filePath/*Commented for PRD by LuoWen*/);'
+        }, {
+            source: '[self removeTargetFile];',
+            target: '// [self removeTargetFile]; /*Commented for PRD by LuoWen*/'
         }];
 
         replaceTmpls(fs, path, targetDir, targetFiles, tmpls);
